57fe7d42706632c28e6f83c09ac807385eca54c8
TheTomozak <tomaszpalczewski99@gmail.com>
2021-03-01 20:23:20 +0100

    Add project

---
+++ learningAmigoscode/src/main/java/pl/tomozak/learningAmigoscode/LearningAmigoscodeApplication.java
@@ -0,0 +1,13 @@
+package pl.tomozak.learningAmigoscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningAmigoscodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningAmigoscodeApplication.class, args);
+	}
+
+}
---
+++ learningAmigoscode/src/test/java/pl/tomozak/learningAmigoscode/LearningAmigoscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tomozak.learningAmigoscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningAmigoscodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
